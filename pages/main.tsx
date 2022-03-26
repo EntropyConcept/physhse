2c9a6516a91b88dd5c172950b78db1804f14b416
TechnoDXStudios <nazari61@yandex.ru>
2022-03-27 01:49:07 +0300

    Fixed timeline. Added dates to timeline

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import {useState} from "react"
 import Button from '../components/Button/button';
-import { Stepper, StepperProps , createStyles, useMantineTheme, Timeline, SegmentedControl } from '@mantine/core';
+import { Stepper, StepperProps , createStyles, useMantineTheme, Timeline, SegmentedControl, Text } from '@mantine/core';
 import { CalendarEvent, Math, ArrowForward, Atom2, GitFork} from 'tabler-icons-react';
 
 const useStyles = createStyles((theme, params, getRef) => ({
@@ -61,21 +61,28 @@ const Home : NextPage = () => {
   const [active, setActive] = useState(0);
   const nextStep = () => setActive((current) => (current < 4 ? current + 1 : current));
   const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+  const [caf, setCaf] = useState('1');
+
   return (
     <div style={{display: 'flex', justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
       <StyledStepper active={active} onStepClick={setActive} breakpoint="sm" >
         <Stepper.Step label="Первый курс">
             <Timeline active={4} bulletSize={24} lineWidth={2} style={{marginLeft: 6}}>
-              <Timeline.Item bullet={<CalendarEvent size={15}/>} title="Первый модуль">
+              <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Первый модуль">
                 Математический анализ<br/>
                 Математический аппарат<br/>
                 Линейная алгебра<br/>
-                Механика
+                Механика<br/>
+                История
               </Timeline.Item>
               <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dashed'>
-
+                <Text variant="link" component="span" inherit>19.12</Text> Механика
+                <br/><Text variant="link" component="span" inherit>22.12</Text> История
+                <br/><Text variant="link" component="span" inherit>24.12</Text> Линейная алгебра
+                <br/><Text variant="link" component="span" inherit>26.12</Text> Математический анализ
+                <br/><Text variant="link" component="span" inherit>27.12</Text> Математический аппарат
               </Timeline.Item>
-              <Timeline.Item bullet={<CalendarEvent size={15}/>} title="Второй модуль">
+              <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Второй модуль">
                 Аналитические приближенные методы<br/>
                 Математический анализ<br/>
                 Электромагнетизм<br/>
@@ -92,7 +99,7 @@ const Home : NextPage = () => {
         </Stepper.Step>
         <Stepper.Step label="Второй курс">
             <Timeline active={4} bulletSize={24} lineWidth={2} style={{marginLeft: 6}}>
-              <Timeline.Item bullet={<CalendarEvent size={15}/>} title="Третий модуль">
+              <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Третий модуль">
                 Вычислительная физика<br/>
                 Математический анализ<br/>
                 Теория вероятностей<br/>
@@ -104,7 +111,7 @@ const Home : NextPage = () => {
               <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dashed'>
 
               </Timeline.Item>
-              <Timeline.Item bullet={<CalendarEvent size={15}/>} title="Четвертый модуль">
+              <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Четвертый модуль">
                 Химия для физиков<br/>
                 Квантовая физика<br/>
                 Математическая физика<br/>
@@ -122,7 +129,7 @@ const Home : NextPage = () => {
         <Stepper.Step label="Третий курс">
           <Timeline active={5} bulletSize={24} lineWidth={2} style={{marginLeft: 6}}>
               <Timeline.Item bullet={<GitFork size={16}/>} title="Кафедра">
-                <SegmentedControl color="blue" data={[
+                <SegmentedControl color="blue" onChange={setCaf} value={caf} data={[
                     {label: "ИСАН", value: "1"},
                     {label: "ИФП", value: "2"},
                     {label: "ИФТТ", value: "3"},
@@ -132,13 +139,13 @@ const Home : NextPage = () => {
                     {label: "ИРЭ", value: "7"},
                   ]}/><br/>
               </Timeline.Item>
-              <Timeline.Item bullet={<CalendarEvent size={15}/>} title="Пятый модуль">
+              <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Пятый модуль">
                 ??
               </Timeline.Item>
               <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dashed'>
 
               </Timeline.Item>
-              <Timeline.Item bullet={<CalendarEvent size={15}/>} title="Шестой модуль">
+              <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Шестой модуль">
                 ??
               </Timeline.Item>
               <Timeline.Item bullet={<Math size={16}/>} title="Итоговая сессия" lineVariant='dashed'>
@@ -152,7 +159,7 @@ const Home : NextPage = () => {
         <Stepper.Step label="Четвертый курс">
           <Timeline active={5} bulletSize={24} lineWidth={2} style={{marginLeft: 6}}>
               <Timeline.Item bullet={<GitFork size={16}/>} title="Кафедра">
-                <SegmentedControl color="blue" data={[
+                <SegmentedControl color="blue" onChange={setCaf} value={caf} data={[
                     {label: "ИСАН", value: "1"},
                     {label: "ИФП", value: "2"},
                     {label: "ИФТТ", value: "3"},
@@ -162,13 +169,13 @@ const Home : NextPage = () => {
                     {label: "ИРЭ", value: "7"},
                   ]}/><br/>
               </Timeline.Item>
-              <Timeline.Item bullet={<CalendarEvent size={15}/>} title="Седьмой модуль">
+              <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Седьмой модуль">
                 ??
               </Timeline.Item>
               <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dashed'>
 
               </Timeline.Item>
-              <Timeline.Item bullet={<CalendarEvent size={15}/>} title="Восьмой модуль">
+              <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Восьмой модуль">
                 ??
               </Timeline.Item>
               <Timeline.Item bullet={<Math size={16}/>}title="Итоговая сессия" lineVariant='dashed'>
