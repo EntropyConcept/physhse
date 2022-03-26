df60447ad90d125193345dbb369a330705d8bf73
TechnoDXStudios <nazari61@yandex.ru>
2022-03-26 23:31:15 +0300

    Now all links to / are pointing to /main removing redirect flickering

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -17,10 +17,10 @@ const Header : FunctionComponent<Props> = (props:Props) => {
           <div className={style.container}>
               <div className={style.left}>
                   <div className={style.label}>
-                      <Link passHref href="/"><img src="/hse3.svg" alt="hse-logo" className={style.logo}/></Link>
+                      <Link passHref href="/main"><img src="/hse3.svg" alt="hse-logo" className={style.logo}/></Link>
                   </div>
                   <div className={style.buttons}>
-                    <Link passHref href="/"><button className={classNames({[style.selected]:props.tab == 0 || dir[1] == "main" || dir[1] == ""})}>Главная</button></Link>
+                    <Link passHref href="/main"><button className={classNames({[style.selected]:props.tab == 0 || dir[1] == "main" || dir[1] == ""})}>Главная</button></Link>
                     <Link passHref href="/c1"><button className={classNames({[style.selected]:props.tab == 1 || dir[1] == "c1"})}>Курс 1</button></Link>
                     <Link passHref href="/c2"><button className={classNames({[style.selected]:props.tab == 2 || dir[1] == "c2"})}>Курс 2</button></Link>
                     <Link passHref href="/c3"><button className={classNames({[style.selected]:props.tab == 3 || dir[1] == "c3"})}>Курс 3</button></Link>
