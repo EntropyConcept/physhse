40d1022b6ad682f87b6d40ff6cdf504c2c6814e3
TechnoDXStudios <nazari61@yandex.ru>
2022-03-27 03:42:24 +0300

    Added timelines for departments

--- pages/main.tsx
+++ pages/main.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import {useState} from "react"
 import Button from '../components/Button/button';
 import { Stepper, StepperProps , createStyles, useMantineTheme, Timeline, SegmentedControl, Text } from '@mantine/core';
-import { CalendarEvent, Math, ArrowForward, Atom2, GitFork} from 'tabler-icons-react';
+import { CalendarEvent, Math, ArrowForward, Atom2, GitFork, GitCommit} from 'tabler-icons-react';
 
 const useStyles = createStyles((theme, params, getRef) => ({
   root: {
@@ -45,17 +45,18 @@ const useStyles = createStyles((theme, params, getRef) => ({
   // },
 }));
 
+
 const segStyle = {
-  display: "grid",
+  display: "flex",
   fontSize: "1rem",
-  maxWidth: "37.8rem",
-  gridTemplateColumns: "repeat(auto-fit, minmax(4.2rem, 1fr))",
-  gap: "-10px",
+  flexWrap: "wrap",
   marginBottom: "-2rem",
   marginTop: "1rem",
-  ".mantine-SegmentedControl-active":{
-    margin: "4px 4px"
-  }
+  maxWidth: "36rem",
+  ".mantine-SegmentedControl-control":{
+    // maxWidth: "4rem"
+    border: "none"
+  },
 }
 
 function StyledStepper(props: StepperProps) {
@@ -86,7 +87,7 @@ const Home : NextPage = () => {
                 Механика<br/>
                 История
               </Timeline.Item>
-              <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dashed'>
+              <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dotted' color="dark">
                 <Text variant="link" component="span" inherit>19.12</Text> Механика
                 <br/><Text variant="link" component="span" inherit>22.12</Text> История
                 <br/><Text variant="link" component="span" inherit>24.12</Text> Линейная алгебра
@@ -101,7 +102,7 @@ const Home : NextPage = () => {
                 Питон<br/>
                 Дифференциальные уравнения
               </Timeline.Item>
-              <Timeline.Item bullet={<Math size={16}/>}title="Итоговая сессия" lineVariant='dashed'>
+              <Timeline.Item bullet={<Math size={16}/>}title="Итоговая сессия" lineVariant='dotted' color="dark">
 
               </Timeline.Item>
               <Timeline.Item bullet={<ArrowForward size={16}/>} title="Второй курс">
@@ -119,7 +120,7 @@ const Home : NextPage = () => {
                 Термодинамика<br/>
                 Тензоры
               </Timeline.Item>
-              <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dashed'>
+              <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dotted' color="dark">
 
               </Timeline.Item>
               <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Четвертый модуль">
@@ -130,7 +131,7 @@ const Home : NextPage = () => {
                 Обработка данных эксперимента<br/>
                 Право
               </Timeline.Item>
-              <Timeline.Item bullet={<Math size={16}/>}title="Итоговая сессия" lineVariant='dashed'>
+              <Timeline.Item bullet={<Math size={16}/>}title="Итоговая сессия" lineVariant='dotted' color="dark">
 
               </Timeline.Item>
               <Timeline.Item bullet={<ArrowForward size={16}/>} title="Третий курс">
@@ -138,7 +139,7 @@ const Home : NextPage = () => {
             </Timeline>
         </Stepper.Step>
         <Stepper.Step label="Третий курс">
-          <Timeline active={5} bulletSize={24} lineWidth={2} style={{marginLeft: 6}}>
+          <Timeline active={24} bulletSize={24} lineWidth={2} style={{marginLeft: 6}}>
               <Timeline.Item bullet={<GitFork size={16}/>} title="Кафедра">
                 <SegmentedControl sx={(theme)=>(segStyle)} color="blue" onChange={setCaf} value={caf} data={[
                     {label: "ИСАН", value: "1"},
@@ -149,6 +150,7 @@ const Home : NextPage = () => {
                     {label: "ИОФ", value: "6"},
                     {label: "ИРЭ", value: "7"},
                     {label: "ПСФ", value: "8"},
+                    {label: "Все", value: "9"}
                   ]}/><br/>
               </Timeline.Item>
               <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Пятый модуль">
@@ -157,48 +159,48 @@ const Home : NextPage = () => {
                 <br/> Атомная и ядерная физика
                 <br/> Введение в астрофизику
                 <br/> Квантовая механика
-                <br/> <hr style={{maxWidth: 100}}/>
-                {caf=="1" /* ИСАН */ && <>
+              </Timeline.Item>
+                {(caf=="1" || caf=="9") /* ИСАН */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИСАН" lineVariant='dotted'>
                       Методы экспериментальной оптики
                 <br/> Квантовая оптика и фотоника
-                </>}
-                {caf=="2" /* ИФП */ && <>
+                </Timeline.Item>
+                }
+                {(caf=="2" || caf=="9") /* ИФП */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИФП" lineVariant='dotted'>
                       Термодинамика и статическая физика
                 <br/> Физика низких температур
                 <br/> Методы измерений
-                </>}
-                {caf=="3" /* ИФТТ */ && <>
+                </Timeline.Item>}
+                {(caf=="3" || caf=="9") /* ИФТТ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИФТТ" lineVariant='dotted'>
                       Дифракционные методы
                 <br/> Введение в физику поверхности
                 <br/> Высоковакуумная техника
                 <br/> Рентген и электромагнетизм
-                </>}
-                {caf=="4" /* ИКИ */ && <>
+                </Timeline.Item>}
+                {(caf=="4" || caf=="9") /* ИКИ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИКИ" lineVariant='dotted'>
                       Космическая и газовая динамика
                 <br/> Солнечная система
                 <br/> Научная литература
-                </>}
-                {caf=="5" /* ИТФ */ && <>
+                </Timeline.Item>}
+                {(caf=="5" || caf=="9") /* ИТФ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИТФ" lineVariant='dotted'>
                       Дополнительная математическая физика
                 <br/> Стохастические процессы
                 <br/> Нелинейные процессы
-                </>}
-                {caf=="6" /* ИОФ */ && <>
+                </Timeline.Item>}
+                {(caf=="6" || caf=="9") /* ИОФ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИОФ" lineVariant='dotted'>
                       Физика твердого тела
                 <br/> Лазерная спектроскопия
-                </>}
-                {caf=="7" /* ИРЭ */ && <>
+                </Timeline.Item>}
+                {(caf=="7" || caf=="9") /* ИРЭ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИРЭ" lineVariant='dotted'>
                       Симметрии в теории групп
                 <br/> Основы магнетизма
                 <br/> Микро- и наноразмерные устройства
                 <br/> Методы электрофизических измерений
-                </>}
-                {caf=="8" /* ПСФ */ && <>
+                </Timeline.Item>}
+                {(caf=="8" || caf=="9") /* ПСФ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ПСФ" lineVariant='dotted'>
                       Психология обучения 
                 <br/> Методы преподавания физики
-                </>}
-              </Timeline.Item>
-              <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dashed'>
+                </Timeline.Item>}
+              <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dotted' color="dark">
 
               </Timeline.Item>
               <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Шестой модуль">
@@ -206,44 +208,43 @@ const Home : NextPage = () => {
                 <br/> Физика мягкой материи
                 <br/> Основы современной физики
                 <br/> Статистическая физика
-                <br/> <hr style={{maxWidth: 100}}/>
-                {caf=="1" /* ИСАН */ && <>
+              </Timeline.Item>
+              {(caf=="1" || caf=="9") /* ИСАН */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИСАН" lineVariant='dotted'>
                       Методы экспериментальной оптики
                 <br/> Квантовая оптика и фотоника
-                </>}
-                {caf=="2" /* ИФП */ && <>
+                </Timeline.Item>}
+                {(caf=="2" || caf=="9") /* ИФП */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИФП" lineVariant='dotted'>
                       Термодинамика и статическая физика
                 <br/> Физика низких температур
-                </>}
-                {caf=="3" /* ИФТТ */ && <>
+                </Timeline.Item>}
+                {(caf=="3" || caf=="9") /* ИФТТ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИФТТ" lineVariant='dotted'>
                       Дифракционные методы
                 <br/> Энергетические спектры
                 <br/> Техника физического эксперимента
-                </>}
-                {caf=="4" /* ИКИ */ && <>
+                </Timeline.Item>}
+                {(caf=="4" || caf=="9") /* ИКИ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИКИ" lineVariant='dotted'>
                       Основы кинетической теории газов
                 <br/> Основы физики плазмы
-                </>}
-                {caf=="5" /* ИТФ */ && <>
+                </Timeline.Item>}
+                {(caf=="5" || caf=="9") /* ИТФ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИТФ" lineVariant='dotted'>
                       Теория хаотических систем
                 <br/> Ланжевеновская динамика и кинетика
-                </>}
-                {caf=="6" /* ИОФ */ && <>
+                </Timeline.Item>}
+                {(caf=="6" || caf=="9") /* ИОФ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИОФ" lineVariant='dotted'>
                       Квантовая теория информации
                 <br/> Зондовая микроскопия
                 <br/> Электронная спектроскопия
-                </>}
-                {caf=="7" /* ИРЭ */ && <>
+                </Timeline.Item>}
+                {(caf=="7" || caf=="9") /* ИРЭ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ИРЭ" lineVariant='dotted'>
                       Наномагнетизм и спинтроника 
                 <br/> Кинетика электронов в твердых телах
-                </>}
-                {caf=="8" /* ПСФ */ && <>
+                </Timeline.Item>}
+                {(caf=="8" || caf=="9") /* ПСФ */ && <Timeline.Item bullet={<GitCommit size={16}/>} title="ПСФ" lineVariant='dotted'>
                       Основы проектирования
                 <br/> Методы преподавания физики
                 <br/> Педагогическая мастерская
-                </>}
-              </Timeline.Item>
-              <Timeline.Item bullet={<Math size={16}/>} title="Итоговая сессия" lineVariant='dashed'>
+                </Timeline.Item>}
+              <Timeline.Item bullet={<Math size={16}/>} title="Итоговая сессия" lineVariant='dotted' color="dark">
 
               </Timeline.Item>
               <Timeline.Item bullet={<ArrowForward size={16}/>} title="Четвертый курс">
@@ -252,7 +253,7 @@ const Home : NextPage = () => {
           </Timeline>
         </Stepper.Step>
         <Stepper.Step label="Четвертый курс">
-          <Timeline active={5} bulletSize={24} lineWidth={2} style={{marginLeft: 6}}>
+          <Timeline active={24} bulletSize={24} lineWidth={2} style={{marginLeft: 6}}>
               <Timeline.Item bullet={<GitFork size={16}/>} title="Кафедра">
                 <SegmentedControl sx={(theme)=>(segStyle)} color="blue" onChange={setCaf} value={caf} data={[
                     {label: "ИСАН", value: "1"},
@@ -263,22 +264,21 @@ const Home : NextPage = () => {
                     {label: "ИОФ", value: "6"},
                     {label: "ИРЭ", value: "7"},
                     {label: "ПСФ", value: "8"},
+                    {label: "Все", value: "9"}
                   ]}/><br/>
               </Timeline.Item>
               <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Седьмой модуль">
                 Физика сплошных сред
-                <br/> <hr style={{maxWidth: 100}}/>
                 Нет данных
               </Timeline.Item>
-              <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dashed'>
+              <Timeline.Item bullet={<Math size={16}/>}title="Промежуточная сессия" lineVariant='dotted' color="dark">
 
               </Timeline.Item>
               <Timeline.Item bullet={<CalendarEvent size={16}/>} title="Восьмой модуль">
                 Электродинамика конденсированных сред
-                <br/> <hr style={{maxWidth: 100}}/>
                 Нет данных
               </Timeline.Item>
-              <Timeline.Item bullet={<Math size={16}/>}title="Итоговая сессия" lineVariant='dashed'>
+              <Timeline.Item bullet={<Math size={16}/>}title="Итоговая сессия" lineVariant='dotted' color="dark">
 
               </Timeline.Item>
               <Timeline.Item bullet={<Atom2 size={16}/>} title="Выпуск">
