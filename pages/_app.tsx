7c825e95596c39be1f2d50c0f92210726a55c5e9
TechnoDXStudios <nazari61@yandex.ru>
2022-03-28 21:23:06 +0300

    added yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout/layout'
+import { YMInitializer } from 'react-yandex-metrika';
 
 function MyApp({ Component, pageProps }: AppProps) {
   
-  return <Layout>
-        <Component {...pageProps} />
-      </Layout>
+  return <>
+    <YMInitializer accounts={[88068304]}/>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>
 }
 export default MyApp
