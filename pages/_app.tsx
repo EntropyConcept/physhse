f69bd55c4ce5cea44e40ab10a115282e37b0b9bd
TechnoDXStudios <nazari61@yandex.ru>
2022-03-28 21:25:26 +0300

    deleted Yandex  metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout/layout'
-import { YMInitializer } from 'react-yandex-metrika';
 
 function MyApp({ Component, pageProps }: AppProps) {
   
   return <>
-    <YMInitializer accounts={[88068304]}/>
     <Layout>
       <Component {...pageProps} />
     </Layout>
