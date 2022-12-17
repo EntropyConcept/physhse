157935b93ba7539699d29c299e33440e68ed4a5d
nazar-si <nazari61@yandex.ru>
2022-12-18 00:12:06 +0300

    Changed organization to be owner

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -10,7 +10,7 @@ const Footer : FunctionComponent<Props> = (props : Props)=>{
     return <>
     <br />
     <div className={style.wrapper}>
-        <span style={{color: "#0008", fontWeight: 500, margin: "0 .5rem"}}>© НИУ ВШЭ 1993–2022</span> <a href="https://github.com/nazar-si">Мой GitHub</a> <a href="https://github.com/nazar-si/physhse">Данный проект</a>
+        <span style={{color: "#0008", fontWeight: 500, margin: "0 .5rem"}}>© НИУ ВШЭ 1993–2022</span> <a href="https://github.com/EntropyConcept">GitHub Команды</a> <a href="https://github.com/EntropyConcept/physhse">Данный проект</a>
     </div>
     </>
 }
